afa31ef1ab075ae2d5d50265037dce89ad7f95ae
syedhussainfaiq <159991416+syedhussainfaiq@users.noreply.github.com>
2024-05-10 21:52:59 +0500

    my first Commit

---
+++ app.js
@@ -0,0 +1,45 @@
+// Question: 1 Install Node>js, Typescript and VS Code on your Laptop.
+// Q 2:
+var Name = "Faiq";
+console.log("Hello ".concat(Name, ", Would you like some Typescript today?"));
+// Q 3:
+var nAme = "Faiq";
+console.log(nAme.toLowerCase());
+console.log(nAme.toUpperCase());
+console.log(nAme.charAt(0).toUpperCase() + nAme.slice(1).toLowerCase());
+// Q 4:
+console.log("Albert Einstin once said, \"A person who never made a mistak never tried \nanything new.\"");
+// Q 5:
+var famous_person = "Albert Einstin";
+var message = "".concat(famous_person, ", once said, \"A person who never made mistak never tried\nanything new.\"");
+console.log(message);
+// Q 6:
+var myName = "\t\n Faiq \t\n";
+console.log(myName);
+console.log(myName.trim());
+// Q 7:
+console.log(5 + 3);
+console.log(4 * 2);
+console.log(10 - 2);
+console.log(16 / 2);
+// Q 8 :
+//(Refer to the answer for Quwstion 7,as it same purpose.)
+// Question 9 :
+var favorate_number = 10;
+console.log("My favorate number ".concat(famous_person, "."));
+// Q 10:
+// Faiq , 2024-05-07
+// This is program a personal message.
+var my_name = "Faiq Hussain";
+console.log("Hell ".concat(myName, ", would you like to learn some Typscript to day?"));
+// Q 11:
+var pesonName = ["Fariz", "Faiq", "Fazir", "Fariha"];
+for (var i = 0; i < pesonName.length; i++) {
+    console.log(pesonName[i]);
+}
+// Q 12:
+var personName = ["Fazir", "Faiq", "Fariha", "Fariz"];
+for (var _i = 0, personName_1 = personName; _i < personName_1.length; _i++) {
+    var name_1 = personName_1[_i];
+    console.log("Hello ".concat(personName, ", would you like to learn some Typscript today?"));
+}
---
+++ app.ts
@@ -0,0 +1,80 @@
+// Question: 1 Install Node>js, Typescript and VS Code on your Laptop.
+
+// Q 2:
+
+let Name: string = "Faiq";
+
+console.log(`Hello ${Name}, Would you like some Typescript today?`)
+
+
+// Q 3:
+let nAme : string = "Faiq";
+
+console.log(nAme.toLowerCase());
+console.log(nAme.toUpperCase());
+console.log(nAme.charAt(0).toUpperCase() + nAme.slice(1).toLowerCase());
+
+
+
+// Q 4:
+
+console.log(`Albert Einstin once said, "A person who never made a mistak never tried 
+anything new."`);
+
+
+// Q 5:
+ 
+let famous_person: string = "Albert Einstin";
+
+let message : string = `${famous_person}, once said, "A person who never made mistak never tried
+anything new."`;
+
+console.log(message);
+
+// Q 6:
+
+let myName: string = "\t\n Faiq \t\n";
+
+console.log(myName);
+console.log(myName.trim());
+
+// Q 7:
+
+console.log( 5 + 3);
+console.log( 4 * 2);
+console.log( 10 - 2);
+console.log( 16 / 2); 
+
+// Q 8 :
+
+//(Refer to the answer for Quwstion 7,as it same purpose.)
+
+
+// Question 9 :
+
+let favorate_number : number = 10;
+
+console.log(`My favorate number ${famous_person}.`);
+
+// Q 10:
+
+
+// Faiq , 2024-05-07
+// This is program a personal message.
+let my_name : string = "Faiq Hussain";
+console.log(`Hell ${myName}, would you like to learn some Typscript to day?`);
+
+// Q 11:
+
+let pesonName : string[] = ["Fariz", "Faiq","Fazir","Fariha"];
+for(let i = 0; i < pesonName.length; i++){
+    console.log(pesonName[i]);
+}
+
+
+// Q 12:
+
+let personName : string[] = ["Fazir", "Faiq","Fariha","Fariz"];
+for (let name of personName) {
+    console.log(`Hello ${personName}, would you like to learn some Typscript today?`);
+}
